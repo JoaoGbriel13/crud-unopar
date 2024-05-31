021d4ba3c8ae790bc179d3a41907bafe2995da57
JoaoGabriel <joaoglima@hotmail.com>
2024-05-31 01:05:11 -0300

    Revert "Projeto final"
    This reverts commit d67d42e801b746b8e0526f37fc5f6e8d48c84f19.

--- src/main/java/com/br/jg/PortfolioFaculdade/Repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.br.jg.PortfolioFaculdade.Repositories;
-
-import com.br.jg.PortfolioFaculdade.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
